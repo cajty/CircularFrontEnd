370d985b1c9a2d6da55f18799b9f02650b68f03d
ayoub belyamane <fdshjgjgffjhd@gmail.com>
2025-03-10 10:58:14 +0100

    add that login

---
+++ src/app/app/features/admin/citys/citys.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-citys',
+  imports: [],
+  templateUrl: './citys.component.html',
+  styleUrl: './citys.component.css'
+})
+export class CitysComponent {
+
+}
