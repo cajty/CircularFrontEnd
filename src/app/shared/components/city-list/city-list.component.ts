a8ac75e9b4a2a5a6ba304ce62a010b90db529dd5
ayoub belyamane <fdshjgjgffjhd@gmail.com>
2025-03-11 11:16:02 +0100

    add that shared component category-list and city-list

---
+++ src/app/shared/components/city-list/city-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-list',
+  imports: [],
+  templateUrl: './city-list.component.html',
+  styleUrl: './city-list.component.css'
+})
+export class CityListComponent {
+
+}
